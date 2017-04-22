51d7cdc9039cf9019f5f7eeb9169fc6e6336c45c
Jermaine Oppong <jeropp00@gmail.com>
2017-04-22 16:09:09 +0100

    Adds TokenService for retrieving persistent token.

--- build/app/app.js
+++ build/app/app.js
@@ -2,6 +2,38 @@
    'use strict';
     angular.module('application', ['ui.router'])
 
+    .factory('TokenService', ['$http', '$rootScope', function($http, $rootScope) {
+        //
+
+        var TOKEN = null;
+        var tokenEndpoint = $rootScope.WebToken + 'helloworld';
+
+        return {
+            retrieve: askToken,
+            get: function() {
+                return TOKEN;
+            }
+        }
+
+        function askToken() {
+            var deferred = $q.defer();
+            $http({
+                method: "GET",
+                url: tokenEndpoint,
+                headers: { 'content-type':'application/json' }
+            })
+            .then(function(response) {
+                TOKEN = response.data;
+                deferred.resolve(response.data);
+            })
+            .catch(function(response) {
+                $log.error('Error retrieving token: ' + response);
+                return $q.reject('Error retrieving token');
+            });
+            return deferred.promise;
+        }
+    }])
+
     .run(['$rootScope', '$state', function($rootScope, $state) {
         // this is available from all across the app
         $rootScope.appName = 'HelloWorld';
--- src/app/components/events/events-controller.js
+++ src/app/components/events/events-controller.js
@@ -1,16 +1,24 @@
 (function() {
     'use strict'
-    angular.module('application').controller('EventsController', ['EventsService', EventsController]);
+    angular.module('application').controller('EventsController', ['EventsService', 'TokenService', EventsController, TokenService]);
 
     function EventsController(EventsService) {
         var self = this;
         
         self.events = null;
         
-        EventsService.getEvents().then(function(events) {
-            console.log(events)
-            self.events = events;  
-        });
+        // Retrieve token
+        TokenService.retrieve().then(function(token) {
+            // Then retrieve events
+            getEvents(token);
+        })
+
+        function getEvents(token) {
+            EventsService.getEvents(token).then(function(events) {
+                console.log(events)
+                self.events = events;  
+            });
+        }
     }
 
 })();
